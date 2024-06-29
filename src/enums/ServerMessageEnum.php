3276d756339b62b04f10d8085edc68fbfb8ce6db
bukarinevgeni <veliknon@gmail.com>
2024-06-29 12:40:58 +0300

    add mising methods

--- src/enums/ServerMessageEnum.php
+++ src/enums/ServerMessageEnum.php
@@ -5,7 +5,7 @@ namespace app\enums;
  * This is an enum class that contains the server status.
  */
 enum ServerMessageEnum: string {
-    case MESSAGE_SUCCESS = 'Successfully connected to the server.';
-    case MESSAGE_FAILURE = 'Failed to connect to the server.';
+    case MESSAGE_SUCCESS = 'Healthy.';
+    case MESSAGE_FAILURE = 'Unhealthy.';
     case MESSAGE_UNKNOWN = 'Status not defined.';
 }
